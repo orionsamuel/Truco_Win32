5013b04faba609b0f2c843c2ff8698540fd72175
orionsamuel <s.oliveira.15@hotmail.com>
2023-12-26 23:32:51 -0300

    Update player.cpp

--- Truco/player.cpp
+++ Truco/player.cpp
@@ -2,6 +2,16 @@
 #include "player.h"
 #include <iostream>
 
+std::string player::getName()
+{
+    return name;
+}
+
+void player::setName(std::string name) const
+{
+    this->name = name;
+}
+
 void player::drawcard(std::shared_ptr<card> card)
 {
     cardList.addCard(card);
